31e7fcfad0df573066c925f5375520eb163fcce5
pavelsof <pavelsof@gmail.com>
2014-02-05 22:04:52 +0200

    Started the start page.

--- racepoint/views.py
+++ racepoint/views.py
@@ -3,11 +3,22 @@ from django.template import RequestContext
 
 
 def show_home(request):
-	return render_to_response(
-		'racepoint/home/home.html',
-		{},
-		context_instance = RequestContext(request)
-	)
+	if 'organiser' in request.session:
+		return render_to_response(
+			'racepoint/home/loggedin.html',
+			{},
+			context_instance = RequestContext(request)
+		)
+	else:
+		return render_to_response(
+			'racepoint/home/login.html',
+			{},
+			context_instance = RequestContext(request)
+		)
+
+
+def show_login(request):
+	pass
 
 
 def list_teams(request):
